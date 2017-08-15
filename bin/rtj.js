a2749c74fd1d0a29dd0f97f8159fe784cb751665
Jacky Jiang <t83714@gmail.com>
2017-08-15 19:38:28 +0930

    init commit

---
+++ bin/rtj.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const fs = require('fs');
+const chalk = require('chalk');
+const meow = require('meow');
+const util = require('util')
+
+const cli=new meow(`
+    Usage  
+      $ rawText2JSON <file>
+`);
+
+if(!cli.input.length || cli.input[0] === 'help') {
+  console.log(cli.help);
+}
+
+let file_name=cli.input[0];
+
+fs.readFile(file_name, (err, data) => {
+  if(err) {throw err;}
+  let output_file_name = file_name.replace(/\.[\w]+$/g, '.json');
+
+  fs.writeFile(output_file_name, JSON.stringify(data.toString()), (err) => {
+    if (err) {throw err;}
+    console.log(chalk.green('JSON file created sucessfully! \n'));
+  });
+});
+
+process.on('uncaughtException', (err) => {
+   console.log(chalk.red('Error: '+err.message));
+});
